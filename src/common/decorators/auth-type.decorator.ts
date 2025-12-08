2037e6344d67af1a22b57e6825c19a8643b5d4c1
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2025-12-08 22:39:50 +0100

    fix: update multiple files

--- src/common/decorators/auth-type.decorator.ts
+++ src/common/decorators/auth-type.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const AUTH_TYPE_KEY = 'authType';
+// auth type
 export const AuthType = (...types: string[]) => SetMetadata(AUTH_TYPE_KEY, types);
