13f5461e3a0764ce0aa52b1a26181d75f9dd0796
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2025-12-09 14:33:53 +0100

    docs: add comments to LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,12 +2,10 @@ import { IsEmail, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  // User email for authentication
   @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   email: string;
 
-  // User password for authentication
   @ApiProperty({ example: 'Password123!' })
   @IsString()
   password: string;
